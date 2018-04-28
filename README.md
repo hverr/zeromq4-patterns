4b44492648aa51b900a10d88db3fde6a78c9d391
Henri Verroken <henriverroken@gmail.com>
2018-04-28 12:31:24 +0200

    Update README with badges and add example to haddock

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 zeromq4-clone-pattern
 =====================
 
+[![Travis](https://travis-ci.org/hverr/zeromq4-clone-pattern.svg?branch=master)](https://travis-ci.org/hverr/zeromq4-clone-pattern)
+[![Hackage](https://img.shields.io/hackage/v/zeromq4-clone-pattern.svg?maxAge=2592000)](https://hackage.haskell.org/package/zeromq4-clone-pattern)
+[![Stackage Nightly](http://stackage.org/package/zeromq4-clone-pattern/badge/nightly)](http://stackage.org/nightly/package/zeromq4-clone-pattern)
+[![Stackage LTS](http://stackage.org/package/zeromq4-clone-pattern/badge/lts)](http://stackage.org/lts/package/zeromq4-clone-pattern)
+
+
+
 Haskell implementation of the [ZeroMQ Reliable Pub-Sub (Clone) pattern][zeromq-clone].
 
   [zeromq-clone]: http://zguide.zeromq.org/page:all#Reliable-Pub-Sub-Clone-Pattern
--- src/System/ZMQ4/Patterns/Clone.hs
+++ src/System/ZMQ4/Patterns/Clone.hs
@@ -1,3 +1,38 @@
+-- | This module implements the ZeroMQ reliable pub-sub (clone) pattern.
+--
+-- See
+-- <http://zguide.zeromq.org/page:all#Reliable-Pub-Sub-Clone-Pattern
+-- the ZeroMQ guide> for more information.
+--
+-- Example usage
+--
+-- @
+-- import Control.Concurrent (threadDelay)
+-- import Control.Concurrent.MVar
+-- import Control.Concurrent.Async
+--
+-- import Control.Monad (forever)
+--
+-- import System.ZMQ4.Patterns.Clone (server, client)
+--
+-- -- | Produce an infinite stream of numbers
+-- producer :: IO ()
+-- producer = do
+--     chan <- newEmptyMVar
+--     withAsync (server "tcp:\/\/:5009" "tcp:\/\/:5010" chan) $ \\_ ->
+--         let send i = putMVar chan i >> threadDelay (100*1000)
+--         mapM_ send [(1 :: Integer)..]
+--
+-- -- | Consume the stream of numbers
+-- consumer :: IO ()
+-- consumer = do
+--     chan <- newEmptyMVar
+--     withAsync (client "tcp:\/\/127.0.0.1:5009" "tcp:\/\/127.0.0.1:5010" chan) $ \\_ ->
+--         forever $ do
+--             n <- takeMVar chan
+--             print n
+-- @
+--
 module System.ZMQ4.Patterns.Clone (
   -- * Server and client
   server
